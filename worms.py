39b5971286343745564f2b671be3543c3518f8be
flheight <75139846+flheight@users.noreply.github.com>
2024-05-23 13:27:14 +0200

    Update worms.py

--- worms.py
+++ worms.py
@@ -8,37 +8,107 @@ class Worms:
     def load_data(self, data):
         self.data = data
 
-    def __init_clusters(self):
+    def __init_clusters(self, init_nodes):
         kmeans = KMeans(self.out_dim).fit(self.data)
-        var = .01 * np.eye(self.data.shape[1])
-        self.clusters = [np.random.multivariate_normal(center, var, 30) for center in kmeans.cluster_centers_]
+        var = 0.01 * np.eye(self.data.shape[1])
+        self.clusters = [np.random.multivariate_normal(center, var, init_nodes) for center in kmeans.cluster_centers_]
 
-    def learn(self, epochs, lam=.005, lr=.5):
-        self.__init_clusters()
+    def __get_density(self, idxs, shape):
+        density = np.zeros(shape - 1)
+        unique_idxs, unique_idxs_counts = np.unique(idxs, return_counts=True)
+        left_mask = unique_idxs > 0
+        right_mask = unique_idxs < shape - 1
+        density[unique_idxs[left_mask] - 1] += unique_idxs_counts[left_mask]
+        density[unique_idxs[right_mask]] += unique_idxs_counts[right_mask]
+        return density
 
-        sample = self.data[np.random.choice(np.arange(self.data.shape[0]), 256)]
-        density = [np.mean(np.exp(-np.sum((sample[:, np.newaxis, :] - chain)**2 / (2 * .2**2), axis=2)), axis=0) for chain in self.clusters]
+    def __mh(self, worm_idx, direction, n_samples, alpha1=0.9, alpha2=0.85):
+        init_cost = self.loss(self.clusters, n_samples)
+        self.cost.append(init_cost)
 
-        for _ in range(epochs):
-            x = self.data[np.random.randint(self.data.shape[0])]
+        snapshot = self.clusters.copy()
+        var = 0.01 * np.eye(self.data.shape[1])
 
-            segments = [chain[1:] - chain[:-1] for chain in self.clusters]
-            diff = [x - chain[:-1] for chain in self.clusters]
+        if direction == 0:
+            new = np.random.multivariate_normal(self.clusters[worm_idx][0], var, 1)
+            snapshot[worm_idx] = np.vstack((new, snapshot[worm_idx]))
+        else:
+            new = np.random.multivariate_normal(self.clusters[worm_idx][-1], var, 1)
+            snapshot[worm_idx] = np.vstack((snapshot[worm_idx], new))
 
-            projections = [np.einsum('ij,ij->i', seg, df) / np.einsum('ij,ij->i', seg, seg) for seg, df in zip(segments, diff)]
-            projections = [np.clip(proj[:, np.newaxis], 0, 1) for proj in projections]
-            dists = [np.einsum('ij,ij->i', df - proj * seg, df - proj * seg) for df, proj, seg in zip(diff, projections, segments)]
+        last_cost = self.loss(snapshot, n_samples)
+        if last_cost / init_cost < alpha1:
+            return snapshot
 
-            winner_worm = np.argmin([np.min(dist) for dist in dists])
-            winner_idx = np.argmin(dists[winner_worm])
-            weight = projections[winner_worm][winner_idx, 0]
-            weights = np.array([[weight], [1 - weight]])
+        if self.clusters[worm_idx].shape[0] <= 2:
+            return self.clusters
 
-            sample = self.data[np.random.choice(np.arange(self.data.shape[0]), 256)]
+        snapshot = self.clusters.copy()
 
-            diffs = sample[:, np.newaxis, :] - self.clusters[winner_worm][winner_idx:winner_idx + 2]
-            density[winner_worm][winner_idx:winner_idx + 2] = np.mean(np.exp(-np.einsum('ijk,ijk->ij', diffs, diffs) / (2 * .2**2)), axis=0)
+        if direction == 0:
+            snapshot[worm_idx] = snapshot[worm_idx][1:]
+        else:
+            snapshot[worm_idx] = snapshot[worm_idx][:-1]
 
-            self.clusters[winner_worm][winner_idx:winner_idx + 2] += (x - self.clusters[winner_worm][winner_idx:winner_idx + 2]) * lr * weights
-            self.clusters[winner_worm][:-1] += lam * lr * segments[winner_worm] / (1 + density[winner_worm][:-1, np.newaxis])
-            self.clusters[winner_worm][1:] -= lam * lr * segments[winner_worm] / (1 + density[winner_worm][1:, np.newaxis])
+        last_cost = self.loss(snapshot, n_samples)
+        if last_cost / init_cost < alpha2:
+            return snapshot
+
+        return self.clusters
+
+    def learn(self, epochs, init_nodes, lam, lr, batch_size=16, mh=50):
+        self.lam = lam
+        self.__init_clusters(init_nodes)
+        self.cost = []
+
+        for epoch in range(epochs):
+            indices = np.random.randint(self.data.shape[0], size=batch_size)
+            x_batch = self.data[indices]
+
+            diffs = [x_batch - worm[:, np.newaxis] for worm in self.clusters]
+            dists = [np.einsum('ijk,ijk->ij', df, df) for df in diffs]
+
+            min_dists = np.array([np.min(dt, axis=0) for dt in dists])
+            winner_worm_idxs = np.argmin(min_dists, axis=0)
+            winner_idxs = np.array([np.argmin(dists[winner_worm_idxs[i]][:, i]) for i in range(x_batch.shape[0])])
+
+            for winner_worm_idx, winner_worm_count in zip(*np.unique(winner_worm_idxs, return_counts=True)):
+                segments = self.clusters[winner_worm_idx][1:] - self.clusters[winner_worm_idx][:-1]
+                density = self.__get_density(winner_idxs[winner_worm_idxs == winner_worm_idx], self.clusters[winner_worm_idx].shape[0])
+                cumdensity1 = np.cumsum(density)
+                cumdensity2 = np.cumsum(density[::-1])
+                d = batch_size * (density + cumdensity1[-1])
+                segments *= self.lam * lr * winner_worm_count / d.reshape(-1, 1)
+                self.clusters[winner_worm_idx][:-1] += segments * cumdensity2.reshape(-1, 1)
+                self.clusters[winner_worm_idx][1:] -= segments * cumdensity1.reshape(-1, 1)
+
+            for i in range(batch_size):
+                winner_worm = winner_worm_idxs[i]
+                winner_idx = winner_idxs[i]
+                self.clusters[winner_worm][winner_idx] += diffs[winner_worm][winner_idx, i] * lr / batch_size
+
+            if epoch % mh == 0:
+                for k in range(self.out_dim):
+                    self.clusters = self.__mh(k, 0, n_samples=batch_size)
+                    self.clusters = self.__mh(k, 1, n_samples=batch_size)
+
+    def loss(self, clusters, n_samples):
+        x_batch = self.data[np.random.randint(self.data.shape[0], size=n_samples)]
+        diffs = [x_batch - worm[:, np.newaxis] for worm in self.clusters]
+        dists = [np.einsum('ijk,ijk->ij', df, df) for df in diffs]
+        min_dists = np.array([np.min(dt, axis=0) for dt in dists])
+        winner_worm_idxs = np.argmin(min_dists, axis=0)
+        winner_idxs = np.array([np.argmin(dists[winner_worm_idxs[i]][:, i]) for i in range(x_batch.shape[0])])
+
+        mse = np.mean([dists[winner_worm_idxs[i]][winner_idx, i] for i, winner_idx in enumerate(winner_idxs)])
+
+        prior_error = 0
+        for winner_worm_idx, winner_worm_count in zip(*np.unique(winner_worm_idxs, return_counts=True)):
+            segments = self.clusters[winner_worm_idx][1:] - self.clusters[winner_worm_idx][:-1]
+            density = self.__get_density(winner_idxs[winner_worm_idxs == winner_worm_idx], self.clusters[winner_worm_idx].shape[0])
+            cumdensity1 = np.cumsum(density)
+            cumdensity2 = np.cumsum(density[::-1])
+            d = n_samples * (density + cumdensity1[-1])
+            prior_error += self.lam * np.mean(np.einsum('ij,ij->i', segments, segments) / d.reshape(-1, 1))
+
+        return mse + prior_error
